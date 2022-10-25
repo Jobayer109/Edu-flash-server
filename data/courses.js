a331b840f871e091fb4d4c7e7f122d508801733e
Nightowlam <nightowl3.00am@gmail.com>
2022-10-25 17:14:50 +0600

    initial commit

---
+++ data/courses.js
@@ -0,0 +1,27 @@
+[
+  {
+    id: "01",
+    name: "HTML",
+  },
+  {
+    id: "02",
+    name: "CSS",
+  },
+  {
+    id: "03",
+    name: "Javascript",
+  },
+  {
+    id: "04",
+    name: "React",
+  },
+  {
+    id: "05",
+    name: "Next js",
+  },
+  {
+    id: "06",
+    name: "",
+  }
+  
+];
