6580db7d7628232b3b702a608f4c01dd50c040d5
Nightowlam <nightowl3.00am@gmail.com>
2022-10-25 18:19:54 +0600

    couses data send as response

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ const cors = require("cors");
 const port = process.env.port || 5000;
 
 app.use(cors());
+const courses = require("./data/courses");
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const perCourse = courses.find((course) => course.id === id);
+  res.send(perCourse);
+});
+
 app.get("/", (req, res) => {
   res.send("Trust me, Server is running");
 });
-
 app.listen(port, () => {
   console.log(`Project app is running on port ${port}`);
 });
